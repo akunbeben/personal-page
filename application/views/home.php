e2b7463f0aefb356474120682bbc17549c5cc2e9
akunbeben <46495960+akunbeben@users.noreply.github.com>
2019-08-26 04:37:18 +0800

    Commit

---
+++ application/views/home.php
@@ -0,0 +1,198 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>style.css">
+    <link href="https://fonts.googleapis.com/css?family=Hind|Lobster&display=swap" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300' rel='stylesheet' type='text/css'>
+    <link href="<?= base_url('assets/'); ?>fontawesome/css/all.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/29a5325f7a.js"></script>
+
+    <title>Freelance PHP Developer Banjarmasin (REST APIs, Codeigniter)</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg fixed-top" id="akunbeben-nav-color">
+        <div class="container">
+            <a class="navbar-brand" id="akunbeben-nav" href="#">@akunbeben</a>
+            <button id="akunbeben-nav-button" class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon" id="akunbeben-nav-button"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto">
+                    <a class="navbar-brand" id="akunbeben-nav" href="https://instagram.com/akunbeben"><i
+                            class="fab fa-instagram"></i></a>
+                    <a class="navbar-brand" id="akunbeben-nav" href="https://github.com/akunbeben"><i
+                            class="fab fa-github"></i></a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <section>
+        <div class="jumbotron jumbotron-fluid" id="akunbeben-jumbotron-container">
+            <div class="container">
+                <div class="container" id="akunbeben-jumbotron">
+                    <h1 class="display-4 text-center" id="display-4">Freelance Fullstack PHP Developer</h1>
+                    <hr>
+                    <p class="lead text-center" id="lead">I am Benny Rahmat, from Banjarbaru, South Kalimantan,
+                        Indonesia. With experience in front-end (HTML5, CSS3, Javascript)
+                        and back-end
+                        (REST
+                        APIs, Codeigniter) I help create interactive solutions for a variety of clients. Available
+                        09.00am - 05.00pm phone / email / whatsapp.
+                    </p>
+                    <hr>
+                    <div class="col-md-8 col-sm" id="akunbeben-img-fluid">
+                        <img src="<?= base_url('assets/images/') . 'code.png'; ?>" class="img-fluid"
+                            alt="Responsive image">
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 col-lg mt-4">
+                            <div class="card text-center" id="akunbeben-card-j">
+                                <div class="card-body">
+                                    <h5 class="card-title">Front-End</h5>
+                                    <p class="card-text">As a web developer, I have experience in HTML5 and CSS3
+                                        techniques working with jQuery and Bootstrap.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg mt-4">
+                            <div class="card text-center" id="akunbeben-card-j">
+                                <div class="card-body">
+                                    <h5 class="card-title">Back-End</h5>
+                                    <p class="card-text">Utilising PHP frameworks such as Codeigniter, laravel or
+                                        creating native PHP code,
+                                        including REST APIs.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <h1 class="display-5 text-center" id="display-5">My Skills</h1>
+                    <hr>
+                    <div class="row justify-content-between">
+                        <div class="col-md-4 col-lg text-center">
+                            <i id="akunbeben-item" class="fab fa-html5 fa-5x"></i>
+                            <h4>Responsive Design</h4>
+                            <p>By using bootstrap I ensure most projects work cross device out of the box.</p>
+                        </div>
+                        <div class="col-md-4 col-lg text-center">
+                            <i id="akunbeben-item" class="fas fa-cogs fa-5x"></i>
+                            <h4>Build with Scalability</h4>
+                            <p>I can help advise the best methodology to ensure your project grows with you.</p>
+                        </div>
+                        <div class="col-md-4 col-lg text-center">
+                            <i id="akunbeben-item" class="fas fa-star fa-5x"></i>
+                            <h4>24/7 Support</h4>
+                            <p>Sometimes building the site is half the work, i'm always around to provide ongoing
+                                support.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <h1 class="display-6 text-center" id="display-6">My Works</h1>
+            <hr>
+            <div class="row justify-content-between">
+                <div class="col-md-4 col-lg text-center mt-2">
+                    <img src="<?= base_url('assets/images/') . 'pos-cafe.png'; ?>" class="img-fluid mb-2"
+                        alt="Responsive image">
+                    <h4><a id="akunbeben-link" href="https://pos-cafe.herokuapp.com/">Point Of Sales - Cafe</a></h4>
+                    <p>Link above just for demo the app. Point of sales included invoicing system, Reporting,
+                        Reservations.</p>
+                </div>
+                <div class="col-md-4 col-lg text-center mt-2">
+                    <img src="<?= base_url('assets/images/') . 'xtring.png'; ?>" class="img-fluid mb-2"
+                        alt="Responsive image">
+                    <h4><a id="akunbeben-link" href="https://github.com/akunbeben/xtring">xTring Projects Manager</a>
+                    </h4>
+                    <p>Experimental open-source app that can manage projects, task, and version control.</p>
+                </div>
+                <div class="col-md-4 col-lg text-center mt-2">
+                    <img src="<?= base_url('assets/images/') . 'pmp2l.png'; ?>" class="img-fluid mb-2"
+                        alt="Responsive image">
+                    <h4><a id="akunbeben-link" href="https://pmp2l.kalselprov.go.id">PMP2L - Kalimantan Selatan</a>
+                    </h4>
+                    <p>Website of PMP2L Kalimantan Selatan with custom content management.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <h1 class="display-6 text-center" id="display-6">Get in touch</h1>
+            <hr>
+            <div class="row justify-content-between">
+                <div class="col-md-4 col-lg text-center mt-2">
+                    <p id="akunbeben-link"><strong>Ready to start your next project with me? Give me a call or send an
+                            email and i will get
+                            back
+                            to you as soon as possible!.</strong></p>
+                </div>
+            </div>
+            <div class="row justify-content-between">
+                <div class="col-md-4 col-lg text-center">
+                    <i id="akunbeben-item" class="fas fa-phone fa-5x"></i>
+                </div>
+                <div class="col-md-4 col-lg text-center">
+                    <i id="akunbeben-item" class="fas fa-envelope fa-5x"></i>
+                </div>
+                <div class="col-md-4 col-lg text-center">
+                    <i id="akunbeben-item" class="fas fa-map-marker fa-5x"></i>
+                </div>
+            </div>
+            <div class="row justify-content-between">
+                <div class="col-md-4 col-lg text-center">
+                    <a id="akunbeben-link" href="tel:082253054008">+62 822 53054008</a>
+                </div>
+                <div class="col-md-4 col-lg text-center">
+                    <a id="akunbeben-link" href="mailto:akunbeben@gmail.com">akunbeben@gmail.com</a>
+                </div>
+                <div class="col-md-4 col-lg text-center">
+                    <a id="akunbeben-link"
+                        href="https://www.google.com/maps/place/TOKO+BAJU+%22KEKE%22/@-3.4265776,114.7273695,19z/data=!4m13!1m7!3m6!1s0x2de682c593a1ef5d:0x46581585104f01b4!2sJl.+Sukamara,+Landasan+Ulin+Bar.,+Kec.+Liang+Anggang,+Kota+Banjar+Baru,+Kalimantan+Selatan+70723!3b1!8m2!3d-3.4311057!4d114.7278357!3m4!1s0x0:0x6293d8d3560d7681!8m2!3d-3.4267069!4d114.7276282"
+                        target="_blank">Banjarbaru</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div id="akunbeben-footer" class="col-lg text-center">
+                <p><strong>Copyright &copy; 2019 Designed and Developed by </strong><a id="akunbeben-nav"
+                        href="https://instagram.com/akunbeben">akunbeben</a></p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
